640336575fc8d9125ba0a9091d3389a327b75efc
Ivan Geevo <dbowgaming@gmail.com>
2025-05-24 15:06:33 +0300

    fixed datagen

---
+++ src/main/java/org/ivangeevo/NomadsRestModDataGenerator.java
@@ -0,0 +1,21 @@
+package org.ivangeevo;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import org.ivangeevo.nomads_rest.datagen.ModBlockTagProvider;
+import org.ivangeevo.nomads_rest.datagen.ModItemTagProvider;
+import org.ivangeevo.nomads_rest.datagen.ModLootTableProvider;
+import org.ivangeevo.nomads_rest.datagen.ModRecipeProvider;
+
+public class NomadsRestModDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+        pack.addProvider(ModBlockTagProvider::new);
+        pack.addProvider(ModItemTagProvider::new);
+        pack.addProvider(ModRecipeProvider::new);
+        pack.addProvider(ModLootTableProvider::new);
+    }
+}
